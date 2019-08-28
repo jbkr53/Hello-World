39db7321e87c17a3daaaf1618807724e57a3aa0e
James Kirkland <jameskirkland@Jamess-MacBook-Pro.local>
2019-08-28 12:39:32 -0500

    First commit

---
+++ week2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  week2
+//
+//  Created by James Kirkland on 8/28/19.
+//  Copyright © 2019 James Kirkland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
